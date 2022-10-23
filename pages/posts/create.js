536d8898ed76b67e20af63a8b01a4fc76871ee2c
sak_shima <sak_shima@worksid.co.jp>
2022-10-23 16:50:50 +0900

    画像の新規登録

--- pages/posts/create.js
+++ pages/posts/create.js
@@ -1,9 +1,9 @@
 // pages/index.js
-import { Authenticator } from '@aws-amplify/ui-react';
-import { Amplify, API, Auth, withSSRContext } from 'aws-amplify';
-import awsExports from '../../src/aws-exports';
-import { createPost } from '../../src/graphql/mutations';
-import styles from '../../styles/Home.module.css';
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify, API, Auth, Storage, withSSRContext } from "aws-amplify";
+import awsExports from "../../src/aws-exports";
+import { createPost } from "../../src/graphql/mutations";
+import styles from "../../styles/Home.module.css";
 
 Amplify.configure({ ...awsExports, ssr: true });
 
@@ -13,33 +13,36 @@ async function handleCreatePost(event) {
   const form = new FormData(event.target);
 
   try {
+    if (!!form.get("image")) {
+      const file = form.get("image");
+      await Storage.put(file.name, file);
+    }
     const { data } = await API.graphql({
-      authMode: 'AMAZON_COGNITO_USER_POOLS',
+      authMode: "AMAZON_COGNITO_USER_POOLS",
       query: createPost,
       variables: {
         input: {
-          title: form.get('title'),
-          content: form.get('content')
-        }
-      }
+          title: form.get("title"),
+          content: form.get("content"),
+            image: form.get("image") ? form.get("image").name : null,
+        },
+      },
     });
 
     window.location.href = `/posts/${data.createPost.id}`;
   } catch ({ errors }) {
-    console.error(...errors);
+    console.error(errors);
     throw new Error(errors[0].message);
   }
 }
 
-export default function Home({ }) {
+export default function Home({}) {
   return (
     <div className={styles.container}>
-
       <main className={styles.main}>
         <h1>ユーザー名のブログを新規作成</h1>
 
         <div className={styles.grid}>
-
           <div className={styles.card}>
             <h3 className={styles.title}>New Post</h3>
 
@@ -59,6 +62,11 @@ export default function Home({ }) {
                     defaultValue="I built an Amplify app with Next.js!"
                     name="content"
                   />
+                </fieldset>
+
+                <fieldset>
+                  <legend>Image</legend>
+                  <input type="file" name="image" />
                 </fieldset>
 
                 <button>Create Post</button>
