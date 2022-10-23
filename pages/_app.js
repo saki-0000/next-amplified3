b9b702c1cb0cf6a4236dd3076f035a5ec366fc2a
sak_shima <sak_shima@worksid.co.jp>
2022-10-23 19:43:28 +0900

    ソースコードのハイライト、行数

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import '@aws-amplify/ui-react/styles.css';
 import Layout from './components/Layout';
+import 'prismjs/themes/prism-okaidia.css';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 
 function MyApp({ Component, pageProps }) {
   return (
