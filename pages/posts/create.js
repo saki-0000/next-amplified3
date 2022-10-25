e6c7070b7d6fb4c06c0032515f58cd887ccac38f
sak_shima <sak_shima@worksid.co.jp>
2022-10-25 22:06:54 +0900

    認証の方法を変更

--- pages/posts/create.js
+++ pages/posts/create.js
@@ -1,6 +1,6 @@
 // pages/index.js
-import { Authenticator } from "@aws-amplify/ui-react";
-import { Amplify, API, Auth, Storage, withSSRContext } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify, API, Auth, Storage } from "aws-amplify";
 import awsExports from "../../src/aws-exports";
 import { createPost } from "../../src/graphql/mutations";
 import styles from "../../styles/Home.module.css";
@@ -24,7 +24,7 @@ async function handleCreatePost(event) {
         input: {
           title: form.get("title"),
           content: form.get("content"),
-            image: form.get("image") ? form.get("image").name : null,
+          image: form.get("image") ? form.get("image").name : null,
         },
       },
     });
@@ -36,48 +36,47 @@ async function handleCreatePost(event) {
   }
 }
 
-export default function Home({}) {
+function Home({}) {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>ユーザー名のブログを新規作成</h1>
+        <h1>{Auth.user.username ?? ""}のブログを新規作成</h1>
 
         <div className={styles.grid}>
           <div className={styles.card}>
             <h3 className={styles.title}>New Post</h3>
 
-            <Authenticator>
-              <form onSubmit={handleCreatePost}>
-                <fieldset>
-                  <legend>Title</legend>
-                  <input
-                    defaultValue={`Today, ${new Date().toLocaleTimeString()}`}
-                    name="title"
-                  />
-                </fieldset>
+            <form onSubmit={handleCreatePost}>
+              <fieldset>
+                <legend>Title</legend>
+                <input
+                  defaultValue={`Today, ${new Date().toLocaleTimeString()}`}
+                  name="title"
+                />
+              </fieldset>
 
-                <fieldset>
-                  <legend>Content</legend>
-                  <textarea
-                    defaultValue="I built an Amplify app with Next.js!"
-                    name="content"
-                  />
-                </fieldset>
+              <fieldset>
+                <legend>Content</legend>
+                <textarea
+                  defaultValue="I built an Amplify app with Next.js!"
+                  name="content"
+                />
+              </fieldset>
 
-                <fieldset>
-                  <legend>Image</legend>
-                  <input type="file" name="image" />
-                </fieldset>
+              <fieldset>
+                <legend>Image</legend>
+                <input type="file" name="image" />
+              </fieldset>
 
-                <button>Create Post</button>
-                <button type="button" onClick={() => Auth.signOut()}>
-                  Sign out
-                </button>
-              </form>
-            </Authenticator>
+              <button>Create Post</button>
+              <button type="button" onClick={() => Auth.signOut()}>
+                Sign out
+              </button>
+            </form>
           </div>
         </div>
       </main>
     </div>
   );
 }
+export default withAuthenticator(Home);
