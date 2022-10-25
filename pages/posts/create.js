540389c98ee4ef2ce77cd317fb3ecb894adc48af
sak_shima <sak_shima@worksid.co.jp>
2022-10-25 22:29:27 +0900

    見た目を調整

--- pages/posts/create.js
+++ pages/posts/create.js
@@ -39,43 +39,71 @@ async function handleCreatePost(event) {
 function Home({}) {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>{Auth.user.username ?? ""}のブログを新規作成</h1>
+      <div class=" dark:bg-gray-800 bg-white py-6 sm:py-8 lg:py-12">
+        <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+          <div class="mb-10 md:mb-16">
+            <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6 dark:text-white">
+              {Auth.user.username}のブログを新規作成
+            </h2>
 
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h3 className={styles.title}>New Post</h3>
-
-            <form onSubmit={handleCreatePost}>
-              <fieldset>
-                <legend>Title</legend>
-                <input
-                  defaultValue={`Today, ${new Date().toLocaleTimeString()}`}
-                  name="title"
-                />
-              </fieldset>
-
-              <fieldset>
-                <legend>Content</legend>
-                <textarea
-                  defaultValue="I built an Amplify app with Next.js!"
-                  name="content"
-                />
-              </fieldset>
-
-              <fieldset>
-                <legend>Image</legend>
-                <input type="file" name="image" />
-              </fieldset>
-
-              <button>Create Post</button>
-              <button type="button" onClick={() => Auth.signOut()}>
-                Sign out
-              </button>
-            </form>
+            <p class="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto dark:text-white">
+              マークダウンで書いてください。
+              <br />
+              「#目次」をいれた箇所に目次がでます。
+            </p>
           </div>
+
+          <form onSubmit={handleCreatePost} class="max-w-screen-md grid sm:grid-cols-2 gap-4 mx-auto">
+            <div class="sm:col-span-2">
+              <label
+                for="title"
+                class="inline-block text-gray-800 text-sm sm:text-base mb-2 dark:text-white"
+              >
+                title*
+              </label>
+              <input
+                name="title"
+                class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+              />
+            </div>
+
+            <div class="sm:col-span-2">
+              <label
+                for="image"
+                class="inline-block text-gray-800 text-sm sm:text-base mb-2 dark:text-white"
+              >
+                image*
+              </label>
+              <input
+                name="image"
+                type="file"
+                class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+              />
+            </div>
+
+            <div class="sm:col-span-2">
+              <label
+                for="content"
+                class="inline-block text-gray-800 text-sm sm:text-base mb-2 dark:text-white"
+              >
+                content*
+              </label>
+              <textarea
+                name="content"
+                class="w-full h-64 bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+              ></textarea>
+            </div>
+
+            <div class="sm:col-span-2 flex justify-between items-center">
+              <button class="inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">
+                Send
+              </button>
+
+              <span class="text-gray-500 text-sm">*Required</span>
+            </div>
+          </form>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
